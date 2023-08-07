f7132aa2adb0704d6e80ca8a9402f038a9394de7
donny <1436339989@qq.com>
2023-08-07 17:03:11 +0800

    lua_nvim

---
+++ nvim/lua/config/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"RRethy/nvim-base16",
+		lazy = true,
+		dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+	},
+	{
+		"justb3a/vim-smarties",
+		lazy = true,
+	},
+}
