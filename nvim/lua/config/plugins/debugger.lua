969055643c7ef60de361214358b0580553cacdab
donny <1436339989@qq.com>
2023-08-10 10:28:49 +0800

    Update debugger.lua

--- nvim/lua/config/plugins/debugger.lua
+++ nvim/lua/config/plugins/debugger.lua
@@ -33,19 +33,44 @@ return {
 			vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
 			vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
 
-			vim.fn.sign_define('DapBreakpoint',
-				{ text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-			vim.fn.sign_define('DapBreakpointCondition',
-				{ text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-			vim.fn.sign_define('DapBreakpointRejected',
-				{ text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-			vim.fn.sign_define('DapLogPoint', {
-				text = '',
-				texthl = 'DapLogPoint',
-				linehl = 'DapLogPoint',
-				numhl = 'DapLogPoint'
-			})
-			vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+
+			local dap_breakpoint = {
+				error = {
+					text = "🛑",
+					texthl = "DapBreakpoint",
+					linehl = "DapBreakpoint",
+					numhl = "DapBreakpoint",
+				},
+				condition = {
+					text = '󰟃',
+					texthl = 'DapBreakpoint',
+					linehl = 'DapBreakpoint',
+					numhl = 'DapBreakpoint',
+				},
+				rejected = {
+					text = "󰃤",
+					texthl = "DapBreakpint",
+					linehl = "DapBreakpoint",
+					numhl = "DapBreakpoint",
+				},
+				logpoint = {
+					text = '',
+					texthl = 'DapLogPoint',
+					linehl = 'DapLogPoint',
+					numhl = 'DapLogPoint',
+				},
+				stopped = {
+					text = '󰜴',
+					texthl = 'DapStopped',
+					linehl = 'DapStopped',
+					numhl = 'DapStopped',
+				},
+			}
+			vim.fn.sign_define('DapBreakpoint', dap_breakpoint.error)
+			vim.fn.sign_define('DapBreakpointCondition', dap_breakpoint.condition)
+			vim.fn.sign_define('DapBreakpointRejected', dap_breakpoint.rejected)
+			vim.fn.sign_define('DapLogPoint', dap_breakpoint.logpoint)
+			vim.fn.sign_define('DapStopped', dap_breakpoint.stopped)
 		end
 	}
 }
