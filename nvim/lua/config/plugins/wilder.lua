7b2375498bf1d381422b2b5e4d7b1921fe698d1e
donny <1436339989@qq.com>
2023-08-16 17:53:48 +0800

    Update wilder.lua

--- nvim/lua/config/plugins/wilder.lua
+++ nvim/lua/config/plugins/wilder.lua
@@ -1,5 +1,7 @@
 return {
 	'gelguy/wilder.nvim',
+	-- event = "VeryLazy",
+	keys = { ":" },
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
@@ -11,25 +13,25 @@ return {
 			previous_key = '<S-Tab>',
 		}
 		wilder.set_option('renderer', wilder.popupmenu_renderer(
-			wilder.popupmenu_palette_theme({
-				highlights = {
-					border = 'Normal', -- highlight to use for the border
-				},
-				left = { ' ', wilder.popupmenu_devicons() },
-				right = { ' ', wilder.popupmenu_scrollbar() },
-				border = 'rounded',
-				max_height = '75%',  -- max height of the palette
-				min_height = 0,      -- set to the same as 'max_height' for a fixed height window
-				prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
-				reverse = 0,         -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
-			})
+		wilder.popupmenu_palette_theme({
+			highlights = {
+				border = 'Normal', -- highlight to use for the border
+			},
+			left = { ' ', wilder.popupmenu_devicons() },
+			right = { ' ', wilder.popupmenu_scrollbar() },
+			border = 'rounded',
+			max_height = '75%', -- max height of the palette
+			min_height = 0, -- set to the same as 'max_height' for a fixed height window
+			prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+			reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+		})
 		))
 		wilder.set_option('pipeline', {
 			wilder.branch(
-				wilder.cmdline_pipeline({
-					language = 'vim',
-					fuzzy = 1,
-				}), wilder.search_pipeline()
+			wilder.cmdline_pipeline({
+				language = 'vim',
+				fuzzy = 1,
+			}), wilder.search_pipeline()
 			),
 		})
 	end
