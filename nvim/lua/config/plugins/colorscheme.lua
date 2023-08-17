9fd23c766c7d30de6b17486884ac2493e081b68c
donny <1436339989@qq.com>
2023-08-17 10:04:40 +0800

    Update colorscheme.lua

--- nvim/lua/config/plugins/colorscheme.lua
+++ nvim/lua/config/plugins/colorscheme.lua
@@ -1,16 +1,5 @@
 return {
 	{
-		"RRethy/nvim-base16",
-		lazy = true,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-	},
-	{
-		"justb3a/vim-smarties",
-		lazy = true,
-	},
-	{
 		"joshdick/onedark.vim",
 		lazy = true,
 	}
