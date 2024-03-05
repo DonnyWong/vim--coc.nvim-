06960b91f1afcfbedef8a07c1d2e34bdb01c9d8c
donny <1436339989@qq.com>
2024-03-05 17:13:51 +0800

    Update colorscheme.lua

--- nvim/lua/config/plugins/colorscheme.lua
+++ nvim/lua/config/plugins/colorscheme.lua
@@ -6,5 +6,13 @@ return {
 	{
 		"olimorris/onedarkpro.nvim",
 		priority = 1000, -- Ensure it loads first
+
+		config = function()
+			require("onedarkpro").setup({
+				colors = {
+					fg = "#81c8ba"
+				}
+			})
+		end,
 	}
 }
