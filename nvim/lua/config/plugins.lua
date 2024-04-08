14b61c1ec74242bf1c4fb7304e572989bdf7df4f
donny <1436339989@qq.com>
2024-04-08 13:58:11 +0800

    Add outline.lua

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -30,6 +30,8 @@ require("lazy").setup({
 	require("config.plugins.debugger"),
 	--treesitter
 	require("config.plugins.treesitter"),
+	--outline
+	require("config.plugins.outline"),
 	--多行注释,单行注释gcc，多行注释vgc
 	require("config.plugins.comment"),
 	--Change Surround examples Press cs"'
