9c6235e7856c18d5e27f0423bdcc1abfacc092ed
donny <1436339989@qq.com>
2024-07-14 10:48:10 +0800

    Update compile_run.lua

--- nvim/lua/config/compile_run.lua
+++ nvim/lua/config/compile_run.lua
@@ -10,8 +10,6 @@ local compileRun = function()
 	local ft = vim.bo.filetype
 	if ft == "dart" then
 		vim.cmd(":FlutterRun -d " .. vim.g.flutter_default_device .. " " .. vim.g.flutter_run_args .. "<CR>")
-	elseif ft == "markdown" then
-		vim.cmd(":InstantMarkdownPreview")
 	elseif ft == 'lua' then
 		split()
 		vim.cmd("term luajit %")
